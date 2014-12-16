3bb12d1400c5ef50bb8ec8791ce2c5dbe3348732
Jonathan Finerty <jonathanfinerty@gmail.com>
2014-12-16 22:02:19 +0000

    Updating android studio, and therefore gradle

--- app/src/main/java/com/jonathanfinerty/liquid/presentation/fragments/EnterDateFragment.java
+++ app/src/main/java/com/jonathanfinerty/liquid/presentation/fragments/EnterDateFragment.java
@@ -2,14 +2,19 @@ package com.jonathanfinerty.liquid.presentation.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
 import com.jonathanfinerty.liquid.R;
+
+import java.lang.reflect.Field;
 
 public class EnterDateFragment extends Fragment {
 
